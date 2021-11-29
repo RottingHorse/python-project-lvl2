2d5e9b01950cc584b4e25960757a31d078c8463e
EvilMadSquirrel <minichev.s.l@gmail.com>
2021-11-29 14:57:16 +0300

    add pytest and github action for tests

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,13 @@
+import pytest
+from gendiff import generate_diff
+
+
+@pytest.fixture(name="correct")
+def get_correct():
+    with open("tests/fixtures/diff.txt") as file:
+        correct = file.read()
+    return correct
+
+
+def test_gendiff_plane_json(correct):
+    assert generate_diff("tests/fixtures/test1.json", "tests/fixtures/test2.json") == correct
