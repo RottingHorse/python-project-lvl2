584519d84b8cf64c5a4eb3bee7b0a5c5eaddd179
EvilMadSquirrel <minichev.s.l@gmail.com>
2021-12-08 13:07:09 +0300

    make tests and fixtures for recursive diff

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,16 +2,33 @@ import pytest
 from gendiff import generate_diff
 
 
-@pytest.fixture(name="correct")
-def get_correct():
-    with open("tests/fixtures/diff.txt") as file:
-        correct = file.read()
-    return correct
+@pytest.fixture(name="correct_plain")
+def get_correct_plain():
+    with open("tests/fixtures/diff_plain.txt") as file:
+        correct_plain = file.read()
+    return correct_plain
 
 
-def test_gendiff_plane_json(correct):
-    assert generate_diff("tests/fixtures/test1.json", "tests/fixtures/test2.json") == correct
+@pytest.fixture(name="correct_recursive")
+def get_correct_recursive():
+    with open("tests/fixtures/diff_plain.txt") as file:
+        correct_recursive = file.read()
+    return correct_recursive
 
 
-def test_gendiff_plane_yaml(correct):
-    assert generate_diff("tests/fixtures/test1.yaml", "tests/fixtures/test2.yaml") == correct
+def test_gendiff_plane_json(correct_plain):
+    assert generate_diff("tests/fixtures/test_plain1.json", "tests/fixtures/test_plain2.json") == correct_plain
+
+
+def test_gendiff_plane_yaml(correct_plain):
+    assert generate_diff("tests/fixtures/test_plain1.yaml", "tests/fixtures/test_plain2.yaml") == correct_plain
+
+
+def test_gendiff_recursive_yaml(correct_recursive):
+    assert generate_diff("tests/fixtures/test_recursive1.yaml",
+                         "tests/fixtures/test_recursive2.yaml") == correct_recursive
+
+
+def test_gendiff_recursive_json(correct_recursive):
+    assert generate_diff("tests/fixtures/test_recursive1.json",
+                         "tests/fixtures/test_recursive2.json") == correct_recursive
